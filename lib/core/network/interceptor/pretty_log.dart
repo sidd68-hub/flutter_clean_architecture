b0fe084e75997b6f172e306e0903d5ad04317611
siddharthnahar <siddharthnahar68@gmail.com>
2025-05-27 14:15:03 +0530

    Remove Unused Import

--- lib/core/network/interceptor/pretty_log.dart
+++ lib/core/network/interceptor/pretty_log.dart
@@ -1,6 +1,5 @@
 import 'package:curl_logger_dio_interceptor/curl_logger_dio_interceptor.dart';
 import 'package:dio/dio.dart';
-import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class AppDioLogger {
   static List<Interceptor> get({bool enableLogging = true}) {
--- lib/feature/screen/home_view/presentation/bloc/home_bloc.dart
+++ lib/feature/screen/home_view/presentation/bloc/home_bloc.dart
@@ -1,6 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_clean_architecture/core/network/api_result.dart';
 import 'package:flutter_clean_architecture/feature/screen/home_view/domain/entities/home_user_entities.dart';
 import 'package:flutter_clean_architecture/feature/screen/home_view/domain/use_case/home_screen_use_case.dart';
@@ -25,7 +24,7 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
     try{
       final result = await homeScreenUseCase(page: 1);
       emit(state.copyWith(isLoading: false,user: result));
-    } on ApiFailure catch(e){
+    } on ApiFailure catch(_){
       emit(NoInternetState());
     }
     catch(e){
